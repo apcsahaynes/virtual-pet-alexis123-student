e0bd7fe268bdd70bca37de4b4166b6a21ff8089f
alexis123-student <alexis.martin@jpschools.org>
2026-03-12 10:47:40 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,48 @@
-public class Game {
+public class Game{
+    private String name;
+    private int happinessUp;
+    private int weightDecr;
+    
+    
+    public Game(String name, int happinessUp, 
+    int weightDecr){
+        this.name = name;
+        this.happinessUp = happinessUp;
+        this.weightDecr = weightDecr;
+    }
+    
+    //name
+public String getName (){
+    return name;
+}
+   // happiness
+  public int getHappinessUp(){
+    return happinessUp;
+  }
+  
+//weight
+   public int getWeightDecr(){
+       return weightDecr;
+   }   
+   
+   //winning game
+   public boolean isWinner(){
+       if (Math.random() > 0.5){
+           return true;
+       }
+       else{
+           return false;
+       }
+   }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
